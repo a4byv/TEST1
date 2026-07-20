01398b206b7cc99e66e7ffd733959e7237e371ad
a4byv <abbybueno112@gmail.com>
2026-07-20 11:40:05 +0800

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,22 @@
+number = float(input("enter a number"))
+if number > 0:
+    print("positive")
+
+grade = float(input("enter your grade: "))
+if grade >= 75:
+    print("Passed")
+    
+number = float(input("Enter an integer: "))
+if number % 2 == 0:
+    print("even")
+else
+    print ("odd")
+    
+hours = float(input("eneter the number of hours worked this week"))
+
+if hours > 40:
+    overtime = ("hours rendered")
+    print ("overtime hours rendered: ", 
+           overtime)
+else:
+    print ("No overtime rendered")
